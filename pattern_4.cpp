3251990b1328f661d645bb651b9fbbd142495859
cristopher-bit <vj023456789@gmail.com>
2025-03-14 20:35:34 +0530

    Add files via upload

---
+++ pattern_4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the number of rows for the diamond: ";
+    cin >> n;
+
+    // Print upper part of the diamond
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j < n; j++) {
+            cout << " ";
+        }
+        for (int j = 1; j <= (2 * i - 1); j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // Print lower part of the diamond
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = n; j > i; j--) {
+            cout << " ";
+        }
+        for (int j = 1; j <= (2 * i - 1); j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
